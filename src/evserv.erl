9027f60f6948067f7fabab33c51772931052a7ea
Иван Кравчук <ivan.k@ati.su>
2018-09-17 15:53:24 +0300

    Finish hiding messages, add makefile
    P.S. It doesn't work

--- src/evserv.erl
+++ src/evserv.erl
@@ -119,3 +119,28 @@ subscribe(Pid) ->
         {error, timeout}
     end.
 
+add_event(Name, Description, TimeOut) ->
+    Ref = make_ref(),
+    ?MODULE ! {self(), Ref, {add, Name, Description, TimeOut}},
+    receive
+        {Ref, Msg} -> Msg
+    after 5000 ->
+        {error, timeout}
+    end.
+
+cancel(Name) -> 
+    Ref = make_ref(),
+    ?MODULE ! {self(), Ref, {cancel, Name}},
+    receive
+        {Ref, ok} -> ok
+    after
+        5000 -> {error, timeout}
+    end.
+
+listen(Delay) -> 
+    receive
+        M = {done, _Name, _Description} ->
+            [M | listen(0)]
+    after
+        Delay*1000 -> []
+    end.
